e5c4272796f4d7be07a4bcb30074757e2f8743e2
Simon A. Berger <simberger@gmail.com>
2022-11-21 18:10:10 +0100

    use normal major axis in texcoord projection

--- src/csg/texgen.rs
+++ src/csg/texgen.rs
@@ -3,7 +3,54 @@ use bevy::{
     prelude::{Vec2, Vec3},
 };
 
-use super::Vertex;
+pub enum MajorAxis {
+    None,
+    Xpos,
+    Xneg,
+    Ypos,
+    Yneg,
+    Zpos,
+    Zneg,
+}
+
+impl MajorAxis {
+    pub fn project(&self, v: Vec3) -> Vec2 {
+        match self {
+            MajorAxis::None => Vec2::ZERO,
+            MajorAxis::Xpos | MajorAxis::Xneg => v.zy(),
+            MajorAxis::Ypos | MajorAxis::Yneg => v.xz(),
+            MajorAxis::Zpos | MajorAxis::Zneg => v.xy(),
+        }
+    }
+}
+
+impl From<Vec3> for MajorAxis {
+    fn from(v: Vec3) -> Self {
+        let v_abs = v.abs();
+
+        if v_abs.x >= v_abs.y && v_abs.x >= v_abs.z {
+            if v.x >= 0.0 {
+                MajorAxis::Xpos
+            } else {
+                MajorAxis::Xneg
+            }
+        } else if v_abs.y >= v_abs.x && v_abs.y >= v_abs.z {
+            if v.y >= 0.0 {
+                MajorAxis::Ypos
+            } else {
+                MajorAxis::Yneg
+            }
+        } else if v_abs.z >= v_abs.x && v_abs.z >= v_abs.y {
+            if v.z >= 0.0 {
+                MajorAxis::Zpos
+            } else {
+                MajorAxis::Zneg
+            }
+        } else {
+            MajorAxis::None
+        }
+    }
+}
 
 pub struct Texgen {
     pub translate: Vec2,
@@ -30,14 +77,14 @@ impl Texgen {
         let c2 = (-self.d_rotate).to_radians().cos();
         let t = (-self.d_rotate).to_radians().tan();
 
-        let tc_trans = pos.yz() - self.translate; // TODO: get major axis from normal
+        let tc = MajorAxis::from(normal).project(pos);
+        let tc_trans = tc - self.translate;
         let tc_rot = Vec2::new(
             tc_trans.x * c - tc_trans.y * s,
             tc_trans.x * s + tc_trans.y * c,
         );
         let tc_shear = Vec2::new(tc_rot.x + tc_rot.y * t, tc_rot.y / c2);
         let tc_scale = tc_shear / self.scale;
-
         tc_scale - self.shift
     }
 }
